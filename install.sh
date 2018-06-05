fc102f2dc96d6fa77ed3b1d570b8c603be2a3045
mixn <sutmil@gmail.com>
2018-06-05 20:27:28 +0200

    Adds install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Setting up Mac…"
+
+# Check for Homebrew and install if needed
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make Zsh the default shell environment
+chsh -s $(which zsh)
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install global NPM packages
+npm i -g now yarn gtop spot n pure-prompt
+
+# Install some globals via curl/wget
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
