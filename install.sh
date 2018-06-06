6a2c0504a55f65e7e867fc62233f83eaf2043220
mixn <sutmil@gmail.com>
2018-06-06 17:04:53 +0200

    Adds oh-my-zsh as a dep

--- install.sh
+++ install.sh
@@ -17,15 +17,16 @@ brew bundle
 # Make Zsh the default shell environment
 chsh -s $(which zsh)
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
 # Install global NPM packages
 npm i -g now yarn gtop spot n pure-prompt
 
 # Install some globals via curl/wget
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 # Remove .zshrc from $HOME (if it exists) and symlink the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
