6f4aea13bdfd7c525a49ba2f5cba8a175c57d64b
mixn <sutmil@gmail.com>
2018-06-10 18:26:19 +0200

    Adds serverless.zsh and moves everything automatically generated from .zshrc to serverless.zsh

--- .zshrc
+++ .zshrc
@@ -28,9 +28,11 @@ source $ZSH/oh-my-zsh.sh
 autoload -U promptinit; promptinit
 prompt pure
 
-# Source and load all things used
+# Source and load all things used manually,
+# I prefer the explicitness here over a loop
 . ~/.dotfiles/aliases.zsh
 . ~/.dotfiles/functions.zsh
 . ~/.dotfiles/git.zsh
 . ~/.dotfiles/nvm.zsh
 . ~/.dotfiles/exports.zsh
+. ~/.dotfiles/serverless.zsh
---
+++ serverless.zsh
@@ -0,0 +1,11 @@
+###############################################################################
+# Serverless                                                                  #
+###############################################################################
+
+# Tabtab source for serverless package
+# Uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
+
+# Tabtab source for sls package
+# Uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
