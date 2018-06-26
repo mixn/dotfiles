f66d859d3cd684c9629eda72cb84ef44989f80ef
mixn <sutmil@gmail.com>
2018-06-27 00:18:32 +0200

    git.zsh: Enables diff-so-fancy for all git diffs

--- git.zsh
+++ git.zsh
@@ -9,3 +9,6 @@ git config --global user.name "$GIT_AUTHOR_NAME"
 GIT_AUTHOR_EMAIL="sutmil@gmail.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
+
+# Use diff-so-fancy by default for all git diff commands
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
