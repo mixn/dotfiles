b89bbb5e9b3cb62884406cb6211e970deb44656c
mixn <sutanovac.milos@gmail.com>
2018-06-28 00:55:43 +0200

    antigen.zsh: Adds alias-tips

--- antigen.zsh
+++ antigen.zsh
@@ -5,7 +5,8 @@
 source /usr/local/share/antigen/antigen.zsh
 
 antigen use oh-my-zsh
-antigen bundle zsh-users/zsh-completions
+antigen bundle djui/alias-tips
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen apply
