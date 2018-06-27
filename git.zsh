f701189b37e4a0b12e41e8a1808b6bb2ee59ec55
mixn <sutanovac.milos@gmail.com>
2018-06-27 16:38:46 +0200

    git.zsh: Adds `git forget`

--- git.zsh
+++ git.zsh
@@ -2,7 +2,7 @@
 # Git                                                                         #
 ###############################################################################
 
-# Many, many other Git goodies I use are provided through oh-my-zsh
+# The Regula ™
 GIT_AUTHOR_NAME="mixn"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
@@ -10,5 +10,10 @@ GIT_AUTHOR_EMAIL="sutanovac.milos@gmail.com"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
+# Many, many other Git goodies I use are provided through oh-my-zsh
 # Use diff-so-fancy by default for all git diff commands
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+# `git forget` for quickly discarding unstaged changes
+# https://stackoverflow.com/a/1021384
+git config --global alias.forget 'checkout .'
