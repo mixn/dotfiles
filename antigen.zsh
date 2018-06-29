2f63d08df0c55dcdfb771752c5a7ebfc746df7e9
mixn <sutanovac.milos@gmail.com>
2018-06-29 02:29:12 +0200

    antigen.zsh: Adds default oh-my-zsh plugins

--- antigen.zsh
+++ antigen.zsh
@@ -6,6 +6,9 @@ source /usr/local/share/antigen/antigen.zsh
 
 antigen use oh-my-zsh
 antigen bundle djui/alias-tips
+antigen bundle git
+antigen bundle git-extras
+antigen bundle npm
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-syntax-highlighting
