834fd8b3a8e147036c0625d50b4988729152dc21
mixn <sutanovac.milos@gmail.com>
2018-06-29 23:41:45 +0200

    install.sh: Adds pkg

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ brew bundle
 chsh -s $(which zsh)
 
 # Install global NPM packages
-npm i -g now yarn gtop spot n pure-prompt serverless @vue/cli ember-cli nba-go git-open speed-test fkill-cli is-up-cli
+npm i -g now yarn gtop spot n pure-prompt serverless @vue/cli ember-cli nba-go git-open speed-test fkill-cli is-up-cli pkg
 
 # Install some globals via curl/wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
