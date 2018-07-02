f8834f6623594eabfbe98a74ca976b3c538869b2
mixn <sutanovac.milos@gmail.com>
2018-07-02 04:01:36 +0200

    🏗 Moves symlinks into their own file

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+###############################################################################
+# Dependencies                                                                #
+###############################################################################
+
 # Update Homebrew recipes
 brew update
 
@@ -35,33 +39,11 @@ mv composer.phar /usr/local/bin/composer
 # Symlinks                                                                    #
 ###############################################################################
 
-# Remove .zshrc from $HOME (if it exists) and symlink the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+source .symlinks
 
-# Remove .tmux.conf from $HOME (if it exists) and symlink the .tmux.conf file from the .dotfiles
-rm -rf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-
-# Remove .tmux.conf.local from $HOME (if it exists) and symlink the .tmux.conf.local file from the .dotfiles
-rm -rf $HOME/.tmux.conf.local
-ln -s $HOME/.dotfiles/.tmux.conf.local $HOME/.tmux.conf.local
-
-# Remove aliases.sh from $HOME (if it exists) and symlink the aliases.sh file from the .dotfiles
-rm -rf $HOME/.oh-my-zsh/lib/aliases.zsh
-ln -s $HOME/.dotfiles/aliases.zsh $HOME/.oh-my-zsh/lib/aliases.zsh
-
-# Remove .vimrc from $HOME (if it exists) and symlink the aliases.sh file from the .dotfiles
-rm -rf $HOME/.vimrc
-ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-# Create ~/.vim/backups, ~/.vim/swaps, ~/.vim/undo as used in .vimrc
-md $HOME/.vim/backups $HOME/.vim/swapsv $HOME/.vim/undo
-
-# Remove .hyper.js from $HOME (if it exists) and copy the .hyper.js file from the .dotfiles,
-# because as of this writing, the .hyper.js config can’t be symlinked ¯\_(ツ)_/¯
-# Quite annoying, but… oh well
-rm -rf $HOME/.hyper.js
-cp $HOME/.dotfiles/.hyper.js $HOME/.hyper.js
+###############################################################################
+# macOS                                                                    #
+###############################################################################
 
 # Set macOS preferences
 # Run last because this will reload the shell
