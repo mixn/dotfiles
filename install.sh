25d371c36468b2822a6aadca07e51cba4013e517
mixn <sutanovac.milos@gmail.com>
2018-09-02 15:30:13 +0200

    🔧 install.sh: Orders dep installation

--- install.sh
+++ install.sh
@@ -28,14 +28,16 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Make Zsh the default shell environment
-chsh -s $(which zsh)
-
+# Install node dependencies
 source .npm
 
-# Install some globals via curl/wget
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Make Zsh the default shell environment
+chsh -s $(which zsh)
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
