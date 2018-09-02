475d6af518216eaa4b8546a115c513debff27314
mixn <sutanovac.milos@gmail.com>
2018-09-02 15:18:48 +0200

    🔧 install.sh: Asks for pw upfront

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@
 
 echo "Setting up Mac…"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.install` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Check for Homebrew and install if needed
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
