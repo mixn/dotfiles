d2b0dbe1e45958128e2c16afa13874fd27d9e92c
mixn <sutanovac.milos@gmail.com>
2018-09-05 17:34:00 +0200

    🐛 install.sh: Fixes failed setting of zsh as default

--- install.sh
+++ install.sh
@@ -56,6 +56,7 @@ source .macos
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make Zsh the default shell environment
+sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
 
 ###############################################################################
