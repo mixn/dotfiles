8212756cda28901417d95be22d0e9fadf1e6bb4e
mixn <sutanovac.milos@gmail.com>
2018-09-23 15:55:23 +0200

    🏗 oh-my-zsh/install.sh: Fixes interactive oh-my-zsh install

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -4,8 +4,13 @@
 # oh-my-zsh                                                                   #
 ###############################################################################
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install oh-my-zsh without user interaction
+# Credit: https://goo.gl/uzeKuL
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o $DOTFILES/omz.sh
+sed -i ''  '/echo/d' $DOTFILES/omz.sh
+sed -i ''  '/env zsh/d' $DOTFILES/omz.sh
+sh $DOTFILES/omz.sh
+rm -rf $DOTFILES/omz.sh
 
 # Make Zsh the default shell environment
 sudo sh -c "echo $(which zsh) >> /etc/shells"
