9c4117bfe3ae8ce54b987c2c3e317f0f857106fb
mixn <sutanovac.milos@gmail.com>
2018-11-14 14:29:29 +0100

    🔧 Lazyloads nvm
    This increases shell start-up time by 1-2s, thanks https://bennycwong.github.io/post/speeding-up-oh-my-zsh/

--- nvm/init.zsh
+++ nvm/init.zsh
@@ -1,3 +1,1 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/.dotfiles/nvm/functions.sh
