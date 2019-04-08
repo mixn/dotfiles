9a8718b6a9e58be49ed44284a3063c972731291b
mixn <sutanovac.milos@gmail.com>
2019-04-08 10:19:39 +0200

    💄 City lights 🎶

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -4,9 +4,9 @@ module.exports = {
     fontFamily: '"IBMPlexMono-ExtraLight", "Inconsolata", "Anonymous Pro"',
     cursorBlink: true,
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BEAM',
-		padding: '0 15px',
-    shellArgs: ['--login'],
+    cursorShape: "BEAM",
+    padding: "0 15px",
+    shellArgs: ["--login"],
     env: {},
     bell: false,
     copyOnSelect: true,
@@ -14,28 +14,28 @@ module.exports = {
     hyperTabs: {
       border: true,
       tabIconsColored: true,
-      closeAlign: 'right',
-      activityPulse: false,
-		},
-		hyperBorder: {
-			borderColors: ['#D066D4', '#53A1F7'],
-			borderWidth: '1px'
-		}
+      closeAlign: "right",
+      activityPulse: false
+    },
+    hyperBorder: {
+      borderColors: ["#D066D4", "#53A1F7"],
+      borderWidth: "1px"
+    }
   },
   plugins: [
-		'hyper-ayu',
-    // 'hyper-ayu-mirage',
+    // "hyper-ayu",
+    // "hyper-ayu-mirage",
     // 'hyper-dark-dracula',
-    // 'hyper-snazzy',
-		// 'hyperterm-duotone-darksea',
-		// 'hyper-statusline',
-		'hyperborder',
-		// 'hyper-city-lights',
-    'hyper-search',
-    'hyper-tabs-enhanced',
-    'hypercwd',
-    'hyperlinks',
-    'hyperterm-dibdabs',
-    'hyperterm-paste',
-  ],
+    // "hyper-snazzy",
+    // 'hyperterm-duotone-darksea',
+    // 'hyper-statusline',
+    "hyperborder",
+    "hyper-city-lights",
+    "hyper-search",
+    "hyper-tabs-enhanced",
+    "hypercwd",
+    "hyperlinks",
+    "hyperterm-dibdabs",
+    "hyperterm-paste"
+  ]
 };
