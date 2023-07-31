c69fe2bcbd7d00e57543c7f5fe7ebd56eb0297c4
mixn <sutanovac.milos@gmail.com>
2023-07-31 15:01:21 +0200

    :arrow_up: Upgrades default nvm version

--- nvm/install.sh
+++ nvm/install.sh
@@ -4,4 +4,4 @@
 # nvm                                                                         #
 ###############################################################################
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
