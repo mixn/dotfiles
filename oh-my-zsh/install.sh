bf45e40c18091b6cf5c06c8ce5f82ba1e7b4c9be
mixn <sutanovac.milos@gmail.com>
2023-07-31 15:01:57 +0200

    :technologist: Changes the way oh-my-zsh is installed

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,14 +3,7 @@
 ###############################################################################
 # oh-my-zsh                                                                   #
 ###############################################################################
-
-# Install oh-my-zsh without user interaction
-# Credit: https://goo.gl/uzeKuL
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o $DOTFILES/omz.sh
-sed -i ''  '/echo/d' $DOTFILES/omz.sh
-sed -i ''  '/env zsh/d' $DOTFILES/omz.sh
-sh $DOTFILES/omz.sh
-rm -rf $DOTFILES/omz.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make Zsh the default shell environment
 sudo sh -c "echo $(which zsh) >> /etc/shells"
