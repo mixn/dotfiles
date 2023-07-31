7ff7a8e4dc29a86e3b797380d52657b2593f7a3b
mixn <sutanovac.milos@gmail.com>
2023-07-31 15:02:37 +0200

    :building_construction: Dock updates

--- dockutil/install.sh
+++ dockutil/install.sh
@@ -1,13 +1,13 @@
 dockutil --remove all --no-restart
-dockutil --add '/Applications/Pocket.app' --no-restart
-dockutil --add '/Applications/Bear.app' --no-restart
+dockutil --add '/Applications/Notion.app' --no-restart
 dockutil --add '/Applications/Things3.app' --no-restart
 dockutil --add '/Applications/Slack.app' --no-restart
-dockutil --add '/Applications/Macdown.app' --no-restart
-dockutil --add '/Applications/Hyper.app' --no-restart
+dockutil --add '/Applications/Figma.app' --no-restart
+dockutil --add '/Applications/Pocket.app' --no-restart
+dockutil --add '/Applications/iTerm.app' --no-restart
 dockutil --add '/Applications/Visual Studio Code.app' --no-restart
+dockutil --add '/Applications/Linear.app' --no-restart
 dockutil --add '/Applications/Google Chrome.app' --no-restart
-dockutil --add '' --type spacer --section apps --before Pocket --no-restart
-dockutil --add '' --type spacer --section apps --after Bear --no-restart
-dockutil --add '' --type spacer --section apps --before Hyper --no-restart
+dockutil --add '' --type spacer --section apps --before Notion --no-restart
+dockutil --add '' --type spacer --section apps --before iTerm --no-restart
 killall Dock
