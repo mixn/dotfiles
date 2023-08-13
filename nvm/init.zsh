9fffd479d15effddaa3aa64993a4de50d6fb9930
mixn <sutanovac.milos@gmail.com>
2023-08-13 15:22:53 +0200

    :wrench: Implicitly autoloads nvm again

--- nvm/init.zsh
+++ nvm/init.zsh
@@ -1,2 +1,5 @@
 source ~/.dotfiles/nvm/exports.sh
 source ~/.dotfiles/nvm/functions.sh
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
