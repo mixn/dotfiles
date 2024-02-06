8453dca3d68c5bff0115ed2e78b6e590303b685e
mixn <sutanovac.milos@gmail.com>
2024-02-07 00:37:32 +0100

    :wrench: Updates dockutil script to match current dock

--- dockutil/install.sh
+++ dockutil/install.sh
@@ -1,13 +1,12 @@
 dockutil --remove all --no-restart
-dockutil --add '/Applications/Notion.app' --no-restart
 dockutil --add '/Applications/Things3.app' --no-restart
+dockutil --add '/Applications/Notion.app' --no-restart
 dockutil --add '/Applications/Slack.app' --no-restart
 dockutil --add '/Applications/Figma.app' --no-restart
-dockutil --add '/Applications/Pocket.app' --no-restart
 dockutil --add '/Applications/iTerm.app' --no-restart
-dockutil --add '/Applications/Visual Studio Code.app' --no-restart
 dockutil --add '/Applications/Linear.app' --no-restart
-dockutil --add '/Applications/Google Chrome.app' --no-restart
-dockutil --add '' --type spacer --section apps --before Notion --no-restart
+dockutil --add '/Applications/Cursor.app' --no-restart
+dockutil --add '/Applications/Arc.app' --no-restart
+dockutil --add '' --type spacer --section apps --before Things3 --no-restart
 dockutil --add '' --type spacer --section apps --before iTerm --no-restart
 killall Dock
